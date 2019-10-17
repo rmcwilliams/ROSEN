e3d341bd659535add69d2c99baa770770bca66fd
Ruben McWilliams <30909047+rmcwilliams@users.noreply.github.com>
2019-10-17 16:18:31 -0400

    Created file to allow users to upload files

---
+++ listFiles.php
@@ -0,0 +1,53 @@
+<div class='well well-sm'><a href='../'><big><b>&larr; Go Back</b></big></a></div>
+
+<?php
+    $files = scandir('./');
+    foreach($files as $file) {
+        if ($file != "." && $file != ".." && $file != "index.php")
+        echo"<div class='well well-sm'><a href='$file'><big>$file</big></a></div>";
+    }
+?>
+
+<?php
+// Check if the form was submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Check if file was uploaded without errors
+    if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
+        $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+        $filename = $_FILES["photo"]["name"];
+        $filetype = $_FILES["photo"]["type"];
+        $filesize = $_FILES["photo"]["size"];
+    
+        // Verify file extension
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        //if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
+    
+        // Verify file size - 5MB maximum
+        $maxsize = 5 * 1024 * 1024;
+        if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
+    
+        // Verify MYME type of the file
+        if(in_array($filetype, $allowed)){
+            // Check whether file exists before uploading it
+            if(file_exists("upload/" . $filename)){
+                echo $filename . " is already exists.";
+            } else{
+                move_uploaded_file($_FILES["photo"]["tmp_name"], "./" . $filename);
+                echo "Your file was uploaded successfully.";
+            } 
+        } else{
+            echo "Error: There was a problem uploading your file. Please try again."; 
+        }
+    } else{
+        echo "Error: " . $_FILES["photo"]["error"];
+    }
+}
+?>
+
+<form action="" method="post" enctype="multipart/form-data">
+        <h2>Upload File</h2>
+        <label for="fileSelect">Filename:</label>
+        <input type="file" name="photo" id="fileSelect">
+        <input type="submit" name="submit" value="Upload">
+        <p><strong>Note:</strong> Only .jpg, .jpeg, .gif, .png formats allowed to a max size of 5 MB.</p>
+    </form>
